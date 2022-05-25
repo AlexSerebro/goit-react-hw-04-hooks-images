ebfe0f3fb49778f6bd3e709466f17401e5b6cbc3
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-05-25 18:43:43 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+
+
+let number = 10;
+
+console.log(++number );
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
